c62278635f64247bd174363c950010541d201fd1
Vitaliy Senenko <vitaliy.senenko@gmail.com>
2020-11-14 20:08:00 +0200

    feat: Connect Redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from 'react-redux';
+import store from './lib/redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import TaskList from './components/TaskList';
+
+const App = () => (
+  <Provider store={store}>
+    <TaskList />
+  </Provider>
+);
 
 export default App;
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -2,8 +2,10 @@ import React from "react";
 import PropTypes from 'prop-types';
 
 import Task from "./Task";
+import { connect } from 'react-redux';
+import { archiveTask, pinTask } from '../lib/redux';
 
-export default function TaskList(
+export function PureTaskList(
   {
     loading,
     tasks,
@@ -43,7 +45,7 @@ export default function TaskList(
     return (
       <div className="list-items">
         <div className="wrapper-message">
-          <span className="icon-check" />
+          <span className="icon-check"/>
           <div className="title-message">You have no tasks</div>
           <div className="subtitle-message">Sit back and relax</div>
         </div>
@@ -65,12 +67,22 @@ export default function TaskList(
   )
 }
 
-TaskList.propTypes = {
+PureTaskList.propTypes = {
   loading: PropTypes.bool,
   tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
   onPinTask: PropTypes.func,
   onArchiveTask: PropTypes.func
 };
-TaskList.defaultProps = {
+PureTaskList.defaultProps = {
   loading: false
 };
+
+export default connect(
+  ({ tasks }) => ({
+    tasks: tasks.filter(t => t.state === 'TASK_INBOX' || t.state === 'TASK_PINNED')
+  }),
+  dispatch => ({
+    onArchiveTask: id => dispatch(archiveTask(id)),
+    onPinTask: id => dispatch(pinTask(id))
+  })
+)(PureTaskList);
